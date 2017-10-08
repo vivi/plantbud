947fe54683e327115ff8176a3bce282a18fdfed9
Chris <chriski777@berkeley.edu>
2017-10-08 05:14:48 -0700

    IT WORKS YOOO! Euclidean distance added and other stuff. Berkeley coordinates give correct annual rainfall average

--- weather_NOAA.js
+++ weather_NOAA.js
@@ -3,9 +3,10 @@ var dataset = 'NOAA DataSet/';
 // change so that it's modified by user input
 var latitude = '37.862612';
 var longitude = '-122.261762';
+var ourCoords = [latitude, longitude];
 // Read File line by line and store into key-value pairings
-var stationDict = [];
-var precipDict = [];
+var stationDict = {};
+var precipDict = {};
 
 function retrieveData(fileName, givenDict){
 	return new Promise(function(resolve,reject){
@@ -14,15 +15,20 @@ function retrieveData(fileName, givenDict){
 		});
 		lineReader.on('line', function (line) {
 			var lineArr = line.split(" ").filter(String);
-			givenDict.push({
-				key: lineArr[0],
-				value : [lineArr[1], lineArr[2]]
-			});
+			if (lineArr.length == 2) {
+				givenDict[lineArr[0]] = lineArr[1];		
+			} else {
+				givenDict[lineArr[0]] = [lineArr[1], lineArr[2]];		
+			}
 		}).on('close', function() {
 	  		resolve(givenDict);
 		});
 	});
 };
+
+function euclideanDist(firstCoords, secCoords){
+	return Math.pow((parseFloat(firstCoords[0]) - parseFloat(secCoords[0])),2) + Math.pow((parseFloat(firstCoords[1]) - parseFloat(secCoords[1])),2)
+}
 var statPromise = retrieveData('station.txt', stationDict);
 statPromise.then(function(statDict) {
 	console.log('Station Dict is filled. It has ' + Object.keys(statDict).length + ' entries.');
@@ -30,5 +36,16 @@ statPromise.then(function(statDict) {
 }).then(function(precipDict) {
 	console.log('Precip Dict is filled. It has ' + Object.keys(precipDict).length + ' entries.');
 }).then(function() {
-	
+	var bestStat;
+	var minDist = 100000000000;
+	//finds closest weather station to given coordinates
+	for (key in stationDict) {
+		var currVal = euclideanDist(ourCoords,stationDict[key]);
+		if (currVal < minDist) {
+			minDist = currVal;
+			bestStat = key;
+		}
+	}
+	console.log('Closest station to your coordinates: ' + bestStat);
+	console.log('Average annual rainfall for coordinates ' + '[' + latitude +',' + longitude + ']: ' + parseInt(precipDict[bestStat])/100 + ' inches');
 })
