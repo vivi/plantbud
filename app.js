4f36f745280e2fe9bbd3afd33eda484c856ca1f1
Vivian Fang <vivian98.fang@gmail.com>
2017-10-09 00:07:27 -0700

    Update dependencies

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var gps = require('./routes/gps');
 
 var app = express();
 
@@ -19,10 +21,12 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/gps', gps);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
