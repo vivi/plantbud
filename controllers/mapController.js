fedefb0714c202439e94328c153d0107b2e0faf6
Vivian Fang <vivian98.fang@gmail.com>
2017-10-28 21:44:28 -0700

    Add comments, adjust spacing

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -1,15 +1,17 @@
 var guide = require('./guideController.js');
 
+/* We only display the mapping on a POST request that contains GPS coordinates */
 exports.map_get = function(req, res, next) {
   res.render('map', {
-      title: 'map'
+    title: 'map'
   });
 };
 
+/* Serve the mapping tool */
 exports.map_post = function(req, res, next) {
   var coord = new guide.Coord(req.body.lat, req.body.lon);
   res.render('map', {
-      title: 'map',
-      coord: coord
+    title: 'map',
+    coord: coord
   });
 };
