d8615df678549ab57daf97a67afab6f13cf1dc69
Chris <chriski777@berkeley.edu>
2017-11-05 17:43:14 -0800

    ParseFruits.py started

--- data_mining/parseFruits.py
+++ data_mining/parseFruits.py
@@ -1,11 +1,17 @@
 import json
 import os
-
-fruitDir = '../data/fruits'
+from os import listdir
+from os.path import isfile, join
+fruitDir = '../data/fruits/'
 def readJSON(dir):
 	if os.path.exists(fruitDir):
 		onlyfiles = [f for f in listdir(fruitDir) if isfile(join(fruitDir, f))]
-		print onlyfiles
+		i = 0
+		for file in onlyfiles:
+			with open(fruitDir + file) as data_file:    
+				data = json.load(data_file)
+				if data.get('Soil:'):
+					soilData =  data.get('Soil:')
 	else: 
 		print ('oh no baby wat is u doin')
 readJSON(fruitDir)
