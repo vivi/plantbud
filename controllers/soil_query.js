87f55160bfbe25def2915a70d50a4ab1bbd27cf9
Chris <chriski777@berkeley.edu>
2017-11-19 22:39:05 -0800

    Finished uploading to database along with changes to model to accomodate for NaN values

--- controllers/soil_query.js
+++ controllers/soil_query.js
@@ -7,29 +7,17 @@ function retrieveSoil(coord){
 	request(link, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
 	        var soilProp= JSON.parse(body)['properties'];
-
-	        var allDict = {}
-
-	        var soilPHDict = soilProp['properties'].PHIHOX.M;
-	        // depth to bedrock in cm, can be interpreted as soil depth
-	        var bedrock_depth = soilJSO.properties.BDRICM.M.BDRICM_M;
-	        // approximate texture from fraction of clay, slit, and sand in soil
-	        var clayFract = soilJSON.properties.CLYPPT.M;
-	        var slitFract = soilJSON.properties.SLTPPT.M;
-	        var sandFract = soilJSON.properties.SNDPPT.M;
-	        var avg = 0;
-	        for (var key in soilPHDict) {
-	        	// soil pH values are multiplied by 10 in json 
-	        	avg += soilPHDict[key]/10
-	        }
-	        avg /= Object.keys(soilPHDict).length
-	        console.log(bedrock_depth)
-	        console.log(avg)
-	        console.log(clayFract)
-	        console.log(slitFract)
-	        console.log(sandFract)
-	        console.log(soilPHDict)
-	        return avg
+	        var soilDict = {
+	        	// depth to bedrock in cm, can be interpreted as soil depth
+	        	bedrock_depth : soilProp.BDRICM.M.BDRICM_M,
+	        	// approximate texture from fraction of clay, slit, and sand in soil
+	        	clayFrac : reduceDict(soilProp.CLYPPT.M,Object.keys(soilProp.CLYPPT.M).length, 1),
+	        	slitFrac : reduceDict(soilProp.SLTPPT.M,Object.keys(soilProp.SLTPPT.M).length, 1),
+	        	sandFrac : reduceDict(soilProp.SNDPPT.M,Object.keys(soilProp.SNDPPT.M).length, 1),
+	        	// pH values are multiplied by 10 in database
+	        	phavg : reduceDict(soilProp.PHIHOX.M,Object.keys(soilProp.PHIHOX.M).length, 10)
+	        };
+	        return soilDict
 	     }
 	})
 }
