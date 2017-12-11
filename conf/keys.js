6ce9c9b5f55cc5b0c9078d53a8de325a712ee132
Vivian Fang <vivian98.fang@gmail.com>
2017-12-11 03:18:31 -0800

    Integrate into new login system, only load previous data upon request

--- conf/keys.js
+++ conf/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
+  googleAPIKeyUnrestricted: process.env.GOOGLE_API_KEY_UNRESTRICTED,
   googleAPIKey: process.env.GOOGLE_API_KEY,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- routes/guide.js
+++ routes/guide.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var guide_controller = require('../controllers/guideController.js');
 
+router.get('/stored', guide_controller.guide_stored_get);
+
 router.get('/', guide_controller.guide_get);
 router.post('/', guide_controller.guide_post);
 
