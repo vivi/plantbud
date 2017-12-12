adddabdc3d434efbe1ac8db1e7d345557d319b83
Vivian Fang <vivian98.fang@gmail.com>
2017-12-11 21:30:17 -0800

    Add address box to guide, search link for plant

--- public/javascripts/geolocate.js
+++ public/javascripts/geolocate.js
@@ -25,3 +25,20 @@ function fillPosition(lat, lon) {
     $('#lat').val(lat);
     $('#lon').val(lon);
 }
+
+var autocomplete;
+function fillInAddress() {
+  var coord = autocomplete.getPlace().geometry.location;
+  fillPosition(coord.lat(), coord.lng());
+}
+
+function initAddress() {
+  var input = document.getElementById('address');
+  autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+/* Shows address form */
+function useAddress() {
+  $('#address-wrapper').slideDown(200);
+}
